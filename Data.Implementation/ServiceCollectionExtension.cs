e91ff322c30418a1b7e024c42a81ff2eac380bc2
DanyloKam@gmail.com <DanyloKam@gmail.com>
2020-09-06 13:08:37 +0300

    Test commit

--- Data.Implementation/ServiceCollectionExtension.cs
+++ Data.Implementation/ServiceCollectionExtension.cs
@@ -3,5 +3,6 @@ namespace Data.Implementation
     public static class ServiceCollectionExtension
     {
         //TODO: write extension method to iservicecollection and register dbcontext/identitycore
+        //test commit comment
     }
 }
