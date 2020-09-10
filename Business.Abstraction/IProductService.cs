758a73fecca8cb5f8ba43758c0d96d9b54208162
DanyloKam@gmail.com <DanyloKam@gmail.com>
2020-09-10 15:49:13 +0300

    Add IProductService

--- Business.Abstraction/IProductService.cs
+++ Business.Abstraction/IProductService.cs
@@ -1,7 +1,15 @@
+using System.Collections.Generic;
+using Business.Models.DTO;
+
 namespace Business.Abstraction
 {
     public interface IProductService
     {
-        //TODO: crud
+        void Create(Product product);
+        ICollection<Product> GetAll();
+        Product GetById(int id);
+        void Update(int id);
+        void Delete(Product product);
+
     }
 }
