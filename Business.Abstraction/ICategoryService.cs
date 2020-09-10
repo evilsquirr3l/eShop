0ba4e891b97208acaf9e677d6f19f0efc239b63f
DanyloKam@gmail.com <DanyloKam@gmail.com>
2020-09-10 15:53:37 +0300

    Add ICategoryService

--- Business.Abstraction/ICategoryService.cs
+++ Business.Abstraction/ICategoryService.cs
@@ -1,7 +1,15 @@
+using System.Collections.Generic;
+using Business.Models.DTO;
+
 namespace Business.Abstraction
 {
     public interface ICategoryService
     {
-        //TODO: crud
+        void Create(Category Category);
+        ICollection<Category> GetAll();
+        Category GetById(int id);
+        void Update(Category Category);
+        void Delete(Category Category);
+        void Delete(int id);
     }
 }
