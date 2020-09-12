460ba53b3e1c445d545d724d220a7dacf1e0332f
Serhii Denshchyk <serhii_denshchyk@epam.com>
2020-09-12 12:52:35 +0300

    Add blank spaces

--- Business.Abstraction/IProductService.cs
+++ Business.Abstraction/IProductService.cs
@@ -7,10 +7,13 @@ namespace Business.Abstraction
     public interface IProductService
     {
         Task Create(ProductDto productDto); 
+        
         Task<ProductDto> GetById(int id);
+        
         Task<IEnumerable<ProductDto>> GetAll();
+        
         Task Update(ProductDto productDto);
+        
         Task Delete(ProductDto productDto);
-
     }
 }
