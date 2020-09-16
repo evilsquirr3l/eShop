b9783140c1e4be6ef91ae1f8c06cae66e8f31b1a
DanyloKam@gmail.com <DanyloKam@gmail.com>
2020-09-16 09:54:33 +0300

    Changed AutoMapper Registration

--- Business.Implementation/ServiceCollectionExtension.cs
+++ Business.Implementation/ServiceCollectionExtension.cs
@@ -5,9 +5,12 @@ namespace Business.Implementation
 {
     public static class ServiceCollectionExtension
     {
-        public static IServiceCollection BindMapper(this IServiceCollection services)
+        public static IServiceCollection RegisterBusinessServices(this IServiceCollection services)
         {
-            services.AddTransient<IMapper>();
+            var mapperConfig = new MapperConfiguration(c => c.AddProfile(new AutoMapperProfile()));
+            var mapper = mapperConfig.CreateMapper();
+
+            services.AddSingleton(mapper);
             return services;
         }
     }
