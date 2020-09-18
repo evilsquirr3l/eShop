d89ef9f8eb807b05a22ae8db1181bc4851379417
Serhii Denshchyk <serhii_denshchyk@epam.com>
2020-09-18 20:25:41 +0300

    Register services

--- Business.Implementation/ServiceCollectionExtension.cs
+++ Business.Implementation/ServiceCollectionExtension.cs
@@ -1,4 +1,6 @@
 using AutoMapper;
+using Business.Abstraction;
+using Data.Entities;
 using Microsoft.Extensions.DependencyInjection;
 
 namespace Business.Implementation
@@ -9,8 +11,12 @@ namespace Business.Implementation
         {
             var mapperConfig = new MapperConfiguration(c => c.AddProfile(new AutoMapperProfile()));
             var mapper = mapperConfig.CreateMapper();
-
             services.AddSingleton(mapper);
+            
+            services.AddTransient<ICartService, CartService>();
+            services.AddTransient<ICategoryService, CategoryService>();
+            services.AddTransient<IProductService, ProductService>();
+            
             return services;
         }
     }
