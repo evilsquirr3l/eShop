b1da6f86dcf53b6b0ea1eb810db7d445b245254a
Serhii Denshchyk <serhii_denshchyk@epam.com>
2020-09-19 14:04:55 +0300

    Add route to about page

--- eShop.API/ClientApp/src/app/app.module.ts
+++ eShop.API/ClientApp/src/app/app.module.ts
@@ -30,6 +30,7 @@ import { AboutComponent } from './about/about.component';
       { path: '', component: HomeComponent, pathMatch: 'full' },
       { path: 'counter', component: CounterComponent },
       { path: 'fetch-data', component: FetchDataComponent },
+      { path: 'about', component: AboutComponent },
     ])
   ],
   providers: [],
