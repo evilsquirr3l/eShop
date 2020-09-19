dd45436f26b048790d77ea75d555d9eb8112e6a9
Serhii Denshchyk <serhii_denshchyk@epam.com>
2020-09-19 13:54:00 +0300

    Add about page

--- eShop.API/ClientApp/src/app/app.module.ts
+++ eShop.API/ClientApp/src/app/app.module.ts
@@ -10,6 +10,7 @@ import { HomeComponent } from './home/home.component';
 import { CounterComponent } from './counter/counter.component';
 import { FetchDataComponent } from './fetch-data/fetch-data.component';
 import { AuthorsComponent } from './authors/authors.component';
+import { AboutComponent } from './about/about.component';
 
 @NgModule({
   declarations: [
@@ -18,7 +19,8 @@ import { AuthorsComponent } from './authors/authors.component';
     HomeComponent,
     CounterComponent,
     FetchDataComponent,
-    AuthorsComponent
+    AuthorsComponent,
+    AboutComponent
   ],
   imports: [
     BrowserModule.withServerTransition({ appId: 'ng-cli-universal' }),
