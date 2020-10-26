9cc023dd92dfad7f0a9ccdba59a00039349b2434
DanyloKam@gmail.com <DanyloKam@gmail.com>
2020-10-26 13:43:28 +0200

    Add category controller

--- eShop.API/Controllers/CategoryController.cs
+++ eShop.API/Controllers/CategoryController.cs
@@ -1,14 +1,72 @@
 using System;
+using System.Collections.Generic;
+using System.Threading.Tasks;
+using Business.Abstraction;
+using Business.Models;
 using Microsoft.AspNetCore.Mvc;
 
 namespace eShop.API.Controllers
 {
     public class CategoryController : Controller
     {
-        //TODO: crud
-        public IActionResult Index()
+
+        private readonly ICategoryService _categoryService;
+
+        public CategoryController(ICategoryService categoryService)
         {
-            throw new NotImplementedException();
+            _categoryService = categoryService;
+        }
+
+
+        [HttpGet("{id}")]
+        public async Task<ActionResult<IEnumerable<CategoryDto>>> GetById(int id)
+        {
+            var book = await _categoryService.GetByIdAsync(id);
+
+            if (book == null)
+            {
+                return NotFound();
+            }
+
+            return Ok(book);
+        }
+
+        [HttpPost]
+        public async Task<ActionResult> Create([FromBody] CategoryDto categoryDto)
+        {
+            try
+            {
+                await _categoryService.CreateAsync(categoryDto);
+            }
+            catch (Exception e)
+            {
+                return BadRequest(e.Message);
+            }
+
+            return CreatedAtAction(nameof(Create), new {categoryDto.Id}, categoryDto);
+        }
+
+        [HttpPut]
+        public async Task<ActionResult> Update(CategoryDto categoryDto)
+        {
+            try
+            {
+                await _categoryService.UpdateAsync(categoryDto);
+            }
+            catch (Exception e)
+            {
+                return BadRequest(e.Message);
+            }
+
+            return Ok();
+        }
+
+        [HttpDelete]
+        public async Task<ActionResult> Delete(CategoryDto categoryDto)
+        {
+            await _categoryService.DeleteAsync(categoryDto);
+
+            return Ok();
         }
     }
 }
