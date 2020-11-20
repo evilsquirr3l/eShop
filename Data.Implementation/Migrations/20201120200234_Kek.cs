1397a1a8878e31967ca7a058475a46a4196a6c23
Serhii Denshchyk <serhii_denshchyk@epam.com>
2020-11-20 22:15:27 +0200

    Migrate from ms sql to postgres

---
+++ Data.Implementation/Migrations/20201120200234_Kek.cs
@@ -0,0 +1,17 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace Data.Implementation.Migrations
+{
+    public partial class Kek : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+
+        }
+    }
+}
