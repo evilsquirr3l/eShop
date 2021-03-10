27dbcb9f1423574e0e7400a6a397987a56054076
evilsquirr3l <45493567+evilsquirr3l@users.noreply.github.com>
2021-03-10 23:27:54 +0200

    Revert "feat: add user secrets (#30)" (#31)
    This reverts commit 5c78754943a8649f24774756a9c8a6b3b4f1c57f.

--- Data.Implementation/DataServices.cs
+++ Data.Implementation/DataServices.cs
@@ -1,19 +1,14 @@
 using Microsoft.EntityFrameworkCore;
-using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
-using Npgsql;
 
 namespace Data.Implementation
 {
     public static class DataServices
     {
-        public static IServiceCollection RegisterDataServices(this IServiceCollection services, IConfiguration configuration)
+        public static IServiceCollection RegisterDataServices(this IServiceCollection services, string connectionString)
         {
-            NpgsqlConnectionStringBuilder builder = new NpgsqlConnectionStringBuilder();
-            builder.ConnectionString = configuration.GetConnectionString("DefaultConnection");
-            builder.Username = configuration["UserID"];
-            builder.Password = configuration["Password"];
-            services.AddDbContext<ShopDbContext>(opt => opt.UseNpgsql(builder.ConnectionString));
+            services.AddDbContext<ShopDbContext>(options =>
+                options.UseNpgsql(connectionString));
             
             return services;
         }
