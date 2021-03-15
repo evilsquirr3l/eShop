9edbce5ba529e942530076f1f93a15118fd43153
evilsquirr3l <45493567+evilsquirr3l@users.noreply.github.com>
2021-03-15 21:06:16 +0200

    Extract styles (#34)
    * feat: install react-router-dom
    
    * feat: add material-ui
    
    * feat: add install material-kit-ui
    
    * feat: add routing
    
    * feat: add homepage
    
    * fix: delete package-lock
    
    * fix: install sass
    
    * fix: move browserrouter from index.js to app.tsx
    
    * fix: test fix
    
    * refactor: move styles from app.css to components

--- eShop.API/clientapp/src/App.tsx
+++ eShop.API/clientapp/src/App.tsx
@@ -1,5 +1,5 @@
 import React from 'react';
-import './App.css';
+import './App.scss';
 import NavBar from "./Navbar/Navbar";
 import {BrowserRouter, Route} from "react-router-dom";
 import Home from "./Home/Home";
