acb8668e84308bc21dee4db7367d7febcffacbb6
Serhii Denshchyk <serhii_denshchyk@epam.com>
2021-04-02 19:04:22 +0300

    feat: add cart service tests

--- eShop.UnitTests/ServiceTests/CartServiceTests.cs
+++ eShop.UnitTests/ServiceTests/CartServiceTests.cs
@@ -1,6 +1,5 @@
 ﻿using AutoMapper;
 using Business.Abstraction;
-using Business.Implementation;
 using Business.Implementation.Services;
 using Business.Models;
 using Data.Entities;
@@ -12,160 +11,82 @@ using Moq;
 using NUnit.Framework;
 using System.Collections.Generic;
 using System.Linq;
-using System.Net.Mime;
 using System.Threading.Tasks;
-using System.Xml;
 
 namespace eShop.UnitTests.ServiceTests
 {
+    [TestFixture]
     public class CartServiceTests
     {
-        private static DbContextOptions<ShopDbContext> _options = new DbContextOptionsBuilder<ShopDbContext>()
-            .UseInMemoryDatabase(databaseName: "ShopDb")
-            .Options;
-        private ShopDbContext _dbContext = new ShopDbContext(_options);
-        private IMapper _mapper;
-        private AutoMapperProfile _profile;
         private Mock<AbstractValidator<CartDto>> _validator;
         private Mock<IServiceHelper<Cart>> _helper;
         private CartService _service;
+        private IMapper _mapper;
+        private ShopDbContext _context;
 
         [SetUp]
-        public void Configure()
+        public void SetUp()
         {
-            _profile = new AutoMapperProfile();
             _validator = new Mock<AbstractValidator<CartDto>>();
+            _context = new ShopDbContext(UnitTestsHelper.GetUnitTestDbOptions());
             _helper = new Mock<IServiceHelper<Cart>>();
-            _mapper = new Mapper(new MapperConfiguration(cfg => cfg.AddProfile(_profile)));
-        }
-
-        [SetUpFixture]
-        public class Seeder
-        {
-            public Seeder()
-            {
-            }
-
-            [OneTimeSetUp]
-            public void SetUp()
-            {
-                using (var context = new ShopDbContext(_options))
-                {
-                    var cart = new Cart {Id = 1, Products = new List<Product>(), TotalPrice = 0};
-                    context.Carts.Add(cart);
-                    context.SaveChanges();
-                }
-            }
+            _mapper = UnitTestsHelper.CreateMapperProfile();
+            _service = new CartService(_mapper, _context, _validator.Object, _helper.Object);
         }
 
         [Test]
-        public void GetByIdAsync_ReturnsCorrectType_WhenDbHasRequiredItem()
+        public async Task GetAllAsync_ReturnsAllElements()
         {
-            var actual = _service.GetByIdAsync(1).Result;
+            var numberOfItemsInDatabase = await _context.Carts.CountAsync();
 
-            Assert.IsInstanceOf<CartDto>(actual);
+            _service.GetAllAsync().Result.Count().Should().Be(numberOfItemsInDatabase);
+        }
+        
+        [Test]
+        public async Task GetByIdAsync_ReturnsNotNullIfItemExists()
+        {
+            var actual = await _service.GetByIdAsync(1);
+
+            actual.Should().NotBeNull();
+        }
+        
+        [Test]
+        public async Task GetByIdAsync_ReturnsNullIfItemDoesntExist()
+        {
+            var actual = await _service.GetByIdAsync(1337);
+
+            actual.Should().BeNull();
         }
 
         [Test]
-        public void GetByIdAsync_ReturnsCorrectItem_WhenDbHasRequiredItem()
+        public async Task AddAsync_AddsItemToDatabase()
         {
-            _service = new CartService(_mapper, _dbContext, _validator.Object, _helper.Object);
-            var expected = new CartDto { Id = 1, Products = new List<ProductDto>(), TotalPrice = 0 };
+            var numberOfItemsInDatabase = await _context.Carts.CountAsync();
+            await _service.AddAsync(new CartDto {Products = new List<ProductDto>(), TotalPrice = 1});
 
-            var actual = _service.GetByIdAsync(1).Result;
-
-            actual.Should().BeEquivalentTo(expected);
+            _context.Carts.CountAsync().Result.Should().Be(numberOfItemsInDatabase + 1);
         }
 
         [Test]
-        public void GetByIdAsync_ReturnsNull_WhenDbDoesNotHaveRequiredItem()
+        public async Task DeleteByIdAsync_DeleteWithId1_DeletesItem()
         {
-            _service = new CartService(_mapper, _dbContext, _validator.Object, _helper.Object);
-
-            var actual = _service.GetByIdAsync(0).Result;
-
-            Assert.AreEqual(null, actual);
+            var numberOfItemsInDatabase = await _context.Carts.CountAsync();
+            await _service.DeleteByIdAsync(1);
+            
+            _context.Carts.CountAsync().Result.Should().Be(numberOfItemsInDatabase - 1);
+            _context.Carts.FindAsync(1).Result.Should().BeNull();
         }
 
         [Test]
-        public void GetAllAsync_ReturnsCorrectType_WhenDbHasRequiredItem()
+        public async Task UpdateAsync_UpdateWithId1_ModelIsUpdated()
         {
-            var actual = _service.GetAllAsync().Result;
+            var cartBeforeUpdate = await _context.Carts.AsNoTracking().FirstOrDefaultAsync(c => c.Id == 1);
 
-            Assert.IsInstanceOf<IEnumerable<CartDto>>(actual);
-        }
+            await _service.UpdateAsync(1, new CartDto {Id = 1, Products = new List<ProductDto>(), TotalPrice = 1488});
 
-        [Test]
-        public void GetAllAsync_ReturnsCorrectItem_WhenDbHasRequiredItem()
-        {
-            _service = new CartService(_mapper, _dbContext, _validator.Object, _helper.Object);
-            var expected = new List<CartDto>
-            {
-                new CartDto {Id = 1, Products = new List<ProductDto>(), TotalPrice = 0},
-            };
-
-            var actual = _service.GetAllAsync().Result;
-
-            actual.Should().BeEquivalentTo(expected);
-        }
-      
-        [Test]
-        public async Task CreateAsync_AddsItemToDatabase_WhenDbHDoesNotHaveElementWithTheSameId()
-        {   
-            using (var context = new ShopDbContext(_options))
-            {
-                var service = new CartService(_mapper, _dbContext, _validator.Object, _helper.Object);
-                var cartDto = new CartDto() { Id = 3, Products = new List<ProductDto>(), TotalPrice = 0};
-                var expected = context.Carts.Count() + 1;
-                await service.CreateAsync(cartDto);
-
-                Assert.AreEqual(expected, context.Carts.Count());
-            }
-        }
-
-        [Test] 
-        public async Task DeleteByIdAsync_DeletesItemFromDatabase_WhenDbHasElementWithTheSameId()
-        {
-            using (var context = new ShopDbContext(_options))
-            {
-                var service = new CartService(_mapper, _dbContext, _validator.Object, _helper.Object);
-                var expected = context.Carts.Count() - 1;
-                await service.DeleteByIdAsync(3);
-
-                Assert.AreEqual(expected, context.Carts.Count());
-            }
-        }
-
-        [Test]
-        public async Task UpdateAsync_UpdatesItemInDatabase_WhenDbHasSameElement()
-        {
-            var cart = _dbContext.Carts
-                .FromSqlRaw("SELECT * FROM dbo.Blogs WHERE Id=1").ToList();
-            var service = new CartService(_mapper, _dbContext, _validator.Object, _helper.Object);
-            // check if local is not null 
-                if (cart[0] != null)
-                {
-                    // detach
-                    _dbContext.Entry(cart[0]).State = EntityState.Detached;
-                }
-                // save 
-                _dbContext.SaveChanges();
-
-                CartDto dto = new CartDto
-                {
-                    Id = cart[0].Id,
-                    Products = new List<ProductDto>(),
-                    TotalPrice = cart[0].TotalPrice
-                };
-                dto.TotalPrice = 4;
-               
-
-                await service.UpdateAsync(1, dto);
-                dto = _mapper.Map<CartDto>(_dbContext.Carts
-                    .FromSqlRaw("SELECT * FROM dbo.Blogs WHERE Id=1").ToList()[0]);
-                dto.Id.Should().Be(1);
-                dto.TotalPrice.Should().Be(4);
-
+            var cartAfterUpdate = await _context.Carts.AsNoTracking().FirstOrDefaultAsync(c => c.Id == 1);
+            cartAfterUpdate.Id.Should().Be(cartBeforeUpdate.Id);
+            cartAfterUpdate.TotalPrice.Should().NotBe(cartBeforeUpdate.TotalPrice);
         }
     }
 }
