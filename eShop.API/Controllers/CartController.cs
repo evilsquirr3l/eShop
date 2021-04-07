9e6ab91c2e7aeabbbc8ff6632de0ef8ea8a6b298
Serhii Denshchyk <serhii_denshchyk@epam.com>
2021-04-07 21:06:00 +0300

    feat: add CartController

---
+++ eShop.API/Controllers/CartController.cs
@@ -0,0 +1,68 @@
+using System;
+using System.Collections.Generic;
+using System.Threading.Tasks;
+using Business.Abstraction;
+using Business.Models;
+using Microsoft.AspNetCore.Mvc;
+
+namespace eShop.API.Controllers
+{
+    [Produces("application/json")]
+    [Route("api/[controller]")]
+    [ApiController]
+    public class CartController : ControllerBase
+    {
+        private readonly ICrudInterface<CartDto> _cartService;
+
+        public CartController(ICrudInterface<CartDto> cartService)
+        {
+            _cartService = cartService;
+        }
+
+        [HttpGet]
+        public async Task<ActionResult<IEnumerable<CartDto>>> GetAll() 
+            => Ok(await _cartService.GetAllAsync());
+
+        [HttpGet("{id}")]
+        public async Task<ActionResult<CartDto>> GetById(int id)
+            => Ok(await _cartService.GetByIdAsync(id));
+        
+        [HttpPost]
+        public async Task<ActionResult> Add([FromBody] CartDto cartDto)
+        {
+            try
+            {
+                await _cartService.AddAsync(cartDto);
+            }
+            catch (Exception e)
+            {
+                return BadRequest(e.Message);
+            }
+        
+            return CreatedAtAction(nameof(Add), new {cartDto.Id}, cartDto);
+        }
+        
+        [HttpPut("{id}")]
+        public async Task<ActionResult> Update(int id, CartDto cartDto)
+        {
+            try
+            {
+                await _cartService.UpdateAsync(id, cartDto);
+            }
+            catch (Exception e)
+            {
+                return BadRequest(e.Message);
+            }
+        
+            return Ok();
+        }
+        
+        [HttpDelete("{id}")]
+        public async Task<ActionResult> Delete(int id)
+        {
+            await _cartService.DeleteByIdAsync(id);
+        
+            return Ok();
+        }
+    }
+}
