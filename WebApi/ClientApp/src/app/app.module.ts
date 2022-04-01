e7a05165de341b0d480d2eda9b3667ccb95e1d11
Serhii Denshchyk <serhii.denshchyk@refinitiv.com>
2022-04-01 20:39:14 +0300

    refactor: fix linter errors

--- WebApi/ClientApp/src/app/app.module.ts
+++ WebApi/ClientApp/src/app/app.module.ts
@@ -1,13 +1,13 @@
-import {BrowserModule} from '@angular/platform-browser';
-import {NgModule} from '@angular/core';
-import {FormsModule} from '@angular/forms';
-import {HttpClientModule} from '@angular/common/http';
-import {RouterModule} from '@angular/router';
+import { BrowserModule } from '@angular/platform-browser';
+import { NgModule } from '@angular/core';
+import { FormsModule } from '@angular/forms';
+import { HttpClientModule } from '@angular/common/http';
+import { RouterModule } from '@angular/router';
 
-import {MdbCheckboxModule} from 'mdb-angular-ui-kit/checkbox';
-import {AppComponent} from './app.component';
-import {NavMenuComponent} from './nav-menu/nav-menu.component';
-import {HomeComponent} from './home/home.component';
+import { MdbCheckboxModule } from 'mdb-angular-ui-kit/checkbox';
+import { AppComponent } from './app.component';
+import { NavMenuComponent } from './nav-menu/nav-menu.component';
+import { HomeComponent } from './home/home.component';
 
 @NgModule({
   declarations: [
