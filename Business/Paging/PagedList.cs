d21c879e0a80dfe4cf4db2b45031f1288476af0c
Serhii Denshchyk <serhii.denshchyk@refinitiv.com>
2022-04-09 19:51:18 +0300

    feat: add categories paging

---
+++ Business/Paging/PagedList.cs
@@ -0,0 +1,26 @@
+namespace Business.Paging;
+
+public class PagedList<T> : List<T>
+{
+    public int CurrentPage { get; private set; }
+    public int TotalPages { get; private set; }
+    public int PageSize { get; private set; }
+    public int TotalCount { get; private set; }
+    public bool HasPrevious => CurrentPage > 1;
+    public bool HasNext => CurrentPage < TotalPages;
+
+    private PagedList(List<T> items, int count, int pageNumber, int pageSize)
+    {
+        TotalCount = count;
+        PageSize = pageSize;
+        CurrentPage = pageNumber;
+        TotalPages = (int) Math.Ceiling(count / (double) pageSize);
+
+        AddRange(items);
+    }
+
+    public static PagedList<T> ToPagedList(List<T> items, int count, int pageNumber, int pageSize)
+    {
+        return new PagedList<T>(items, count, pageNumber, pageSize);
+    }
+}
