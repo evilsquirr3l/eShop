391d88e5e1f8d3816d5260c4e619e58203f7d800
Serhii Denshchyk <serhii.denshchyk@refinitiv.com>
2022-04-11 21:52:30 +0300

    feat: add model binding to mocked product

--- WebApi/ClientApp/src/app/eshop/components/product-details/product-details.component.ts
+++ WebApi/ClientApp/src/app/eshop/components/product-details/product-details.component.ts
@@ -1,5 +1,8 @@
 import { Component, OnInit } from '@angular/core';
 import { faCartShopping, faStar, faStarHalf } from '@fortawesome/free-solid-svg-icons';
+import { Product } from "../../models/product";
+import { Category } from "../../models/category";
+import { Discount } from "../../models/discount";
 
 @Component({
   selector: 'app-product-details',
@@ -16,4 +19,35 @@ export class ProductDetailsComponent implements OnInit {
   faStar = faStar;
   faStarHalf = faStarHalf;
   faCart = faCartShopping;
+
+  testDate: Date = new Date();
+
+  testDiscount: Discount = {
+    createdAt: this.testDate,
+    description: "TestDiscountDescription",
+    discountPercent: 10,
+    id: 1,
+    isActive: false,
+    isDeleted: false,
+    modifiedAt: this.testDate,
+    name: "DiscountName"
+  }
+  testCategory: Category = {
+    createdAt: this.testDate, description: "Category Description", id: 1, isDeleted: false, modifiedAt: this.testDate, name: "CategoryName"
+  }
+  testProduct : Product = {
+    category: this.testCategory,
+    categoryId: this.testCategory.id,
+    createdAt: this.testDate,
+    description: "test description",
+    discount: this.testDiscount,
+    discountId: this.testDiscount.id,
+    id: 1,
+    isDeleted: false,
+    modifiedAt: this.testDate,
+    name: "TestProductName",
+    pictureUrl: "https://m.media-amazon.com/images/I/71D9ImsvEtL._UY500_.jpg",
+    price: 228,
+    quantity: 5
+  }
 }
